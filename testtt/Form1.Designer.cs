370c2671d02329bd374b141d2f92177f79e952e9
Виталий Коваль <kkovalvitalikk@gmail.com>
2018-05-09 19:20:57 +0300

    first commit

---
+++ testtt/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace testtt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.textBox_Word = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btn_Upload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(276, 12);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_Word
+            // 
+            this.textBox_Word.Location = new System.Drawing.Point(157, 14);
+            this.textBox_Word.Name = "textBox_Word";
+            this.textBox_Word.Size = new System.Drawing.Size(113, 20);
+            this.textBox_Word.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 186);
+            this.listBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(227, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(285, 164);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(315, 218);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // btn_Upload
+            // 
+            this.btn_Upload.Location = new System.Drawing.Point(397, 217);
+            this.btn_Upload.Name = "btn_Upload";
+            this.btn_Upload.Size = new System.Drawing.Size(75, 23);
+            this.btn_Upload.TabIndex = 5;
+            this.btn_Upload.Text = "Upload";
+            this.btn_Upload.UseVisualStyleBackColor = true;
+            this.btn_Upload.Click += new System.EventHandler(this.btn_Upload_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 261);
+            this.Controls.Add(this.btn_Upload);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox_Word);
+            this.Controls.Add(this.btnBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox textBox_Word;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btn_Upload;
+    }
+}
+
---
+++ testtt/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace testtt
+{
+    public partial class Form1 : Form
+    {
+        string ID = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnDelete.Enabled = false;
+            btn_Upload.Enabled = false;
+        }
+        
+        public static string Reverse(string s)
+        {
+            char[] charArr = s.ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Martin\source\repos\testtt\testtt\BDtesttt.mdf;Integrated Security=True;Connect Timeout=30");
+            string text = string.Empty;
+            string str = string.Empty;
+            int count;
+            int index;
+            List<string> f_text = new List<string>();
+            if (textBox_Word.Text != string.Empty)
+            {
+                OpenFileDialog opf = new OpenFileDialog();
+                opf.Filter = "txt files(*.txt)|*.txt";
+                if (opf.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(opf.FileName);
+                    text = sr.ReadToEnd();
+                    sr.Close();
+                    string[] subsrt = text.Split('.');
+                    for (int i = 0; i < subsrt.Length; i++)
+                    {
+                        if (subsrt[i].Contains(textBox_Word.Text))
+                        {
+                            index = 0;
+                            count = 0;
+                            while ((index = subsrt[i].IndexOf(textBox_Word.Text, index) + 1) != 0)
+                                count++;
+                            str = Reverse(subsrt[i]);
+                            str += ".";
+                            string query = "insert into test (sentence,count) values ('" + str.ToString() + "','" + count.ToString() + "')";
+                            SqlCommand command = new SqlCommand(query, conn);
+                            SqlDataReader reader;
+                            try
+                            {
+                                conn.Open();
+                                reader = command.ExecuteReader();
+                                while (reader.Read())
+                                {
+
+                                }
+                                conn.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            string query2 = "select * from test";
+                            conn.Open();
+                            SqlDataAdapter SDA = new SqlDataAdapter(query2, conn);
+                            DataTable dt = new DataTable();
+                            SDA.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                            conn.Close();
+                            f_text.Add(str);
+                            btn_Upload.Enabled = true;
+                            btnDelete.Enabled = true;
+                        }
+                    }
+                    listBox1.Items.AddRange(f_text.ToArray());
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter the word!!!");
+            }
+
+
+        }
+
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Martin\source\repos\testtt\testtt\BDtesttt.mdf;Integrated Security=True;Connect Timeout=30");
+            string query = "delete from test where Id='" + ID + "'";
+            conn.Open();
+            SqlDataAdapter SDA = new SqlDataAdapter(query, conn);
+            SDA.SelectCommand.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void btn_Upload_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Martin\source\repos\testtt\testtt\BDtesttt.mdf;Integrated Security=True;Connect Timeout=30");
+            string query = "select * from test";
+            conn.Open();
+            SqlDataAdapter SDA = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            SDA.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void dataGridView1_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            ID = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+        }
+    }
+}
